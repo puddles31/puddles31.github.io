1db1d1f1b356678483351f6e87999a27386ec65d
puddles31 <samuelborg31@gmail.com>
2020-03-28 10:42:21 +0000

    Update countdown.js

--- countdown.js
+++ countdown.js
@@ -1,25 +1,3 @@
-var countDownDate = new Date("2020-03-27T23:00:00Z").getTime();
-countdownTick();
-var x = setInterval(countdownTick, 1000);
-
-function countdownTick() {
-  var now = new Date().getTime();
-  var distance = countDownDate - now;
-
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  document.getElementById("countdownTime").innerHTML = days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
-
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("countdownTime").innerHTML = "The finale begins now...";
-  }
-}
-
-
 function back() {
   window.history.back();
 }
